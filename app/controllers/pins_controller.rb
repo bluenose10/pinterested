2a35fbd6133eaa883ec941a698ea0b6f71c12cad
Mark Moran <mark.moran4@btinternet.com>
2015-02-13 23:22:01 +0000

    update index file

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,16 +1,16 @@
  class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :correct_user, only: [:edit, :update, :destroy] 
-
-  respond_to :html
+  before_action :correct_user, only: [:edit, :update, :destroy]   
+   
+   respond_to :html
 
   def index
     @pins = Pin.all
-   end
+  end
 
   def show
-   end
+  end
 
   def new
     @pin = current_user.pins.build
@@ -28,30 +28,32 @@
     end
   end
 
-   def update
+  def update
    if @pin.update(pin_params)
       redirect_to @pin, notice: 'Pin was successfully updated.'
     else
       render action: 'edit'
-     end
-   end
+    end
+  end
 
   def destroy
     @pin.destroy
     redirect_to pins_url
   end
 
-   private
+  private
     def set_pin
       @pin = Pin.find(params[:id])
     end
 
-      def pin_params
-        params.require(:pin).permit(:description, :image)
+    def pin_params
+      params.require(:pin).permit(:description : :image)
     end
-   def correct_user
+
+
+  def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
   end
 
-end 
+end
